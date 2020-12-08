1fdd38abed54c185165f8b974a18057a898bafc2
z03639ag <andrei.ghira@student.manchester.ac.uk>
2020-12-08 01:18:45 +0000

    Adding images and improving the colision system

--- coursework_02/game.py
+++ coursework_02/game.py
@@ -1,7 +1,7 @@
 #1920 x 1080 resolution
 
 #<------- Ghiri Studios ------>
-from tkinter import Tk, Canvas
+from tkinter import Tk, Canvas, PhotoImage
 from time import sleep
 from random import randint as rand
 
@@ -26,9 +26,18 @@ def place_first_obstacle():
 	x = rand(10, 1130)
 	global obstacles
 	obstacles = []
-	obstacles.append( canvas.create_rectangle(0, 0, x, 40, fill="green") )
-	obstacles.append( canvas.create_rectangle(x+150,0 , 1280, 40, fill="green") )
+	obstacles.append( canvas.create_rectangle(0, 0, x, 40, fill="white") )
+	obstacles.append( canvas.create_rectangle(x+150,0 , 1280, 40, fill="white") )
 
+def collision(i):
+	coords1 = canvas.coords(obstacles[i])
+	coords2 = canvas.coords(obstacles[i+1])
+	xy = canvas.coords(player)
+	
+	if ( (xy[0] <= coords1[2] and xy[1] - 30 == coords1[3]) or ( xy[0] >= coords2[0] and xy[1] - 30 == coords2[3] )  ):
+		return True
+
+	return False
 
 def spawning_obstacles():
 	global ok
@@ -43,21 +52,20 @@ def spawning_obstacles():
 
 			canvas.move(obstacles[i], 0, 5) # moving the obstacles to the left
 			canvas.move(obstacles[i+1], 0, 5)
-
-			xy_last1 = canvas.coords(obstacles[i])
-			xy_last2 = canvas.coords(obstacles[i+1])
-			xy = canvas.coords(player)
-			print(i)
-			if ( (xy[0] <= xy_last1[2] and xy[1] == xy_last1[3]) or (xy[0] >= xy_last2[0] and xy[1] == xy_last2[3]) ):
-				return 0
+			
+			if ( collision(i) ):
+				return False
 
 			xy_last = canvas.coords(obstacles[start-1])
+
 			if ( xy_last[1] > 250):
 				start+=2
 				x = rand(10, 1130)
-				obstacles.append(canvas.create_rectangle(0, 0, x, 40, fill="green"))
-				obstacles.append(canvas.create_rectangle(x+150,0 , 1280, 40, fill="green"))
-		return 1
+				obstacles.append(canvas.create_rectangle(0, 0, x, 40, fill="white"))
+				obstacles.append(canvas.create_rectangle(x+150,0 , 1280, 40, fill="white"))
+
+		return True
+
 	else:
 
 		for i in range(0, 6, 2): 
@@ -74,14 +82,12 @@ def spawning_obstacles():
 				xy_obj = canvas.coords(obstacles[i])
 
 				if (xy_obj[1] < 720):
-					canvas.move(obstacles[i], 0, 5)
-					canvas.move(obstacles[i+1], 0, 5)
-					xy_last1 = canvas.coords(obstacles[i])
-					xy_last2 = canvas.coords(obstacles[i+1])
-					xy = canvas.coords(player)
 
-					if ( (xy[0] <= xy_last1[2] and xy[1] == xy_last1[3]) or (xy[0] >= xy_last2[0] and xy[1] == xy_last2[3]) ):
-						return 0
+					canvas.move(obstacles[i], 0, 5) # moving the obstacles to the left
+					canvas.move(obstacles[i+1], 0, 5)
+
+					if ( collision(i) ):
+					 	return False
 
 				else:
 
@@ -91,17 +97,18 @@ def spawning_obstacles():
 			except:
 					if (obstacles[i] not in canvas.find_all() and (xy_last[1] > 250) ):
 						x = rand(10, 1130)
-						obstacles[i]=canvas.create_rectangle(0, 0, x, 40, fill="green")
-						obstacles[i+1]=canvas.create_rectangle(x+150,0 , 1280, 40, fill="green")
-		return 1
+						obstacles[i]=canvas.create_rectangle(0, 0, x, 40, fill="white")
+						obstacles[i+1]=canvas.create_rectangle(x+150,0 , 1280, 40, fill="white")
+
+		return True
 
 
 def running():
 	canvas.pack()
 	canvas.config(bg="black")
+
 	global GameOver
 	GameOver = spawning_obstacles()
-
 	if (GameOver):
 		window.after(100, running)
 
@@ -111,11 +118,15 @@ height =720
 window = setWindowsDimensions(width, height)
 canvas = Canvas(window, width=width, height=height)
 
-player = canvas.create_rectangle(width/2-10, height/2-10, width/2+10, height/2+10, fill="red") # creating the character
+spacecraft = PhotoImage(file="character.png")
+background = PhotoImage(file="pule.png")
+bg = canvas.create_image(0, 0, image=background)
+
+player = canvas.create_image(width/2, height/2, image=spacecraft)
 
 score = 0
 score_text = "Score:" + str(score)
-txt = canvas.create_text(40, 30, fill="white", font="Times 20 italic bold", text=score_text)
+txt = canvas.create_text(40, 30, fill="black", font="Times 20 italic bold", text=score_text)
 
 canvas.bind("<Left>", move_left)
 canvas.bind("<Right>", move_right)
@@ -131,90 +142,3 @@ start = 1
 running()
 
 window.mainloop()
-# def movement(event): # function for the binding key which make the character jump
-# 	xy = canvas.coords(player)
-
-# 	if (event.char == "a"):
-# 		canvas.move(player, -10, 0)
-# 		sleep(0.02)
-
-# 	if (event.char == "d"):
-# 		canvas.move(player, 10, 0)
-# 		sleep(0.02)
-
-# 	window.update()
-
-
-# window=Tk()
-# window.title("Ghiri's Game")
-# canvas = Canvas(window, width=1280, height=720) # creating the map
-# canvas.pack()
-# canvas.config(bg="black")
-
-# window.bind("<Key>", movement) # binding the key
-
-# player = canvas.create_rectangle(500, 500, 520, 520, fill="red") # creating the character
-
-
-# def tubes_movement():
-	# for i in range(0, start):
-	# 	canvas.move(obstacles[i-1], 0, 10) # moving the obstacles to the left
-	# 	canvas.move(obstacles[i], 0, 10)
-	# 	sleep(0.05)
-	# 	window.update()
-
-	# 	xy_last = canvas.coords(obstacles[start-1]) #checking where is the last spawned obstacle so we can spawn another one
-	# 	if ( xy_last[1] > 100):
-	# 		start+=2
-	# 		x = rand(10, 1130)
-	# 		obstacles.append(canvas.create_rectangle(0, 0, x, 40, fill="green"))
-	# 		obstacles.append(canvas.create_rectangle(x+150,0 , 1280, 40, fill="green"))
-
-# ok = 1 # indexes for the first few steps of spawning the obstacles
-# start = 1
-# obstacles = [] 
-# x = rand(10, 1130) # creating random obstacles
-# obstacles.append(canvas.create_rectangle(0, 0, x, 40, fill="green")) # an obstacle is built up from 2 pieces, one coming from the ceiling and another from below
-# obstacles.append(canvas.create_rectangle(x+150,0 , 1280, 40, fill="green"))
-
-# window.after(10, tubes_movement)
-# if True:
-
-# 	if (start == 7): # when all the obstacles are spawned
-# 		ok = 0
-# 		#break
-
-# 	if (ok == 1):#spawning the obstacles
-
-
-	# else: # the obstacles are now spawned and now we are just rotating them
-	# 	for i in range(0, 8, 2): 
-
-	# 		if (i == 0):
-	# 			last_object = 7
-	# 		else:
-	# 			last_object = i-1
-
-	# 		xy_last = canvas.coords(obstacles[last_object])
-
-	# 		try: 
-	# 			xy_obj = canvas.coords(obstacles[i])
-	# 			if (xy_obj[0] > 0):
-	# 				canvas.move(obstacles[i], 0, 5)
-	# 				canvas.move(obstacles[i+1], 0, 5)
-	# 				#sleep(0.01)
-	# 				window.update()
-
-	# 			else:
-	# 				canvas.delete(obstacles[i])
-	# 				canvas.delete(obstacles[i+1])
-	# 		except:
-	# 			if (obstacles[i] not in canvas.find_all() and (xy_last[1] > 100) ):
-	# 				x = rand(10, 1130)
-	# 				obstacles.append(canvas.create_rectangle(0, 0, x, 40, fill="green"))
-	# 				obstacles.append(canvas.create_rectangle(x+150,0 , 1280, 40, fill="green"))
-
-
-#window.mainloop()
-
-
--- coursework_02/game_production.py
+++ coursework_02/game_production.py
@@ -5,69 +5,63 @@ from tkinter import Tk, Canvas
 from time import sleep
 from random import randint as rand
 
-def up(event):
-	x = 0
-	y = -60
-	
+def up(event): # function for the binding key which make the character jump
 	xy = canvas.coords(player)
-	if (xy[1] > 10):
 
-		canvas.move(player, x, y)
+	if (xy[1] > 10): # checking where the character is located so it can't leave the map when going up
+		canvas.move(player, 0, -60)
 		sleep(0.01)
 
 	window.update()
 
 
 window=Tk()
-
-canvas = Canvas(window, width=1280, height=720)
+canvas = Canvas(window, width=1280, height=720) # creating the map
 canvas.pack()
 canvas.config(bg="black")
-window.bind("<Key>", up)
 
-player = canvas.create_rectangle(500, 500, 520, 520, fill="red")
+window.bind("<Key>", up) # binding the key
+
+player = canvas.create_rectangle(500, 500, 520, 520, fill="red") # creating the character
 
 obstacles = [] 
-y = rand(10, 500)
-obstacles.append(canvas.create_rectangle(1240, 0, 1280, y, fill="green"))
+y = rand(10, 500) # creating random obstacles
+obstacles.append(canvas.create_rectangle(1240, 0, 1280, y, fill="green")) # an obstacle is built up from 2 pieces, one coming from the ceiling and another from below
 obstacles.append(canvas.create_rectangle(1240,y+150 , 1280, 720, fill="green"))
-ok = 1
+
+ok = 1 # indexes for the first few steps of spawning the obstacles
 start = 1
+
 while True:
-	# xy = canvas.coords(player)
-	# if (xy[3] < 720):
 
-	# 	canvas.move(player, 0, 4)
-	# 	sleep(0.03)
-	# 	window.update()
-
-	if (start == 7):
+	if (start == 7): # when all the obstacles are spawned
 		ok = 0
 
-	if (ok == 1):
+	if (ok == 1):#spawning the obstacles
+
 		for i in range(0, start):
 
-			xy = canvas.coords(player)
+			xy = canvas.coords(player) # gravity system so the player is going down consatntly
 			if (xy[3] < 720):
 				canvas.move(player, 0, 4)
 				sleep(0.03)
 				window.update()
 
-			canvas.move(obstacles[i-1], -10, 0)
+			canvas.move(obstacles[i-1], -10, 0) # moving the obstacles to the left
 			canvas.move(obstacles[i], -10, 0)
 			#sleep(0.03)
 			window.update()
 
-		xy_last = canvas.coords(obstacles[start-1])
+		xy_last = canvas.coords(obstacles[start-1]) #checking where is the last spawned obstacle so we can spawn another one
 		y = rand(10, 500)
 		if (1240 - xy_last[0] > 150):
 		 	obstacles.append(canvas.create_rectangle(1240, 0, 1280, y, fill="green"))
 		 	obstacles.append(canvas.create_rectangle(1240,y+150 , 1280, 720, fill="green"))
 		 	start += 2
 
-	else:
+	else: # the obstacles are now spawned and now we are just rotating them
 
-		for i in range(0, 8, 2):
+		for i in range(0, 8, 2): 
 
 			if (i == 0):
 				last_object = 7
