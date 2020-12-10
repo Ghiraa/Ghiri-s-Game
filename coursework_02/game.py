02297144ea223f48f5483cec105d8fcd17cd0b84
z03639ag <andrei.ghira@student.manchester.ac.uk>
2020-12-10 00:47:04 +0000

    Boss key + nickname + try again menu

--- coursework_02/game.py
+++ coursework_02/game.py
@@ -9,16 +9,18 @@ from random import randint as rand
 def move_left(event):
 	global player
 	global p
-	xy = canvas.coords(player)
-	if (xy[0] - 45 > 0 and not p):
-		canvas.move(player, -15, 0)
+	if ( player in canvas.find_all() ):
+		xy = canvas.coords(player)
+		if (xy[0] - 45 > 0 and not p):
+			canvas.move(player, -15, 0)
 
 def move_right(event):
 	global player
 	global p
-	xy = canvas.coords(player)
-	if (xy[0] + 45 < 1280 and not p):
-		canvas.move(player, 15, 0)
+	if ( player in canvas.find_all() ):
+		xy = canvas.coords(player)
+		if (xy[0] + 45 < 1280 and not p):
+			canvas.move(player, 15, 0)
 
 def setWindowsDimensions(w, h):
 	window = Tk()
@@ -109,7 +111,7 @@ def spawning_obstacles():
 
 				if (xy_obj[1] < 720):
 
-					canvas.move(obstacles[i], 0, 5) # moving the obstacles to the left
+					canvas.move(obstacles[i], 0, 5) # moving the obstacles to the left               NU UITA SA SCHIMBI VITEZA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 					canvas.move(obstacles[i+1], 0, 5)
 					altitude += 5
 					height_text = "ALTITUDE:" + str(altitude) + "ft"
@@ -134,21 +136,47 @@ def spawning_obstacles():
 
 def pause(event):
 	global p
+	global pause_text
+	global bg_pause
 	if (event.char == 'p'):
 
 		if (p):
 			p = False
 
+			if ( pause_text in canvas.find_all() and bg_pause in canvas.find_all() ):
+				canvas.delete(pause_text)
+				canvas.delete(bg_pause)
+
 		else:
+			bg_pause = canvas.create_rectangle(width/2-250, height/2 - 100, width/2 + 250, height/2 + 100, fill="#80bfff")
+			pause_text = canvas.create_text(width/2, height/2, text = "GAME PAUSED", fill="white", font="OCRB 40 bold")
 			p = True 
 
+def bosskey(event):
+	global b
+	global boss_image
+	global boss
+
+	if (b):
+		b = False
+		if ( boss in canvas.find_all() ):
+			canvas.delete(boss)
+
+	else:
+		boss_image = PhotoImage(file="boss2.PNG")
+		boss = canvas.create_image(0, 0, image=boss_image, anchor="nw")
+		b = True 
 
 def getname():
 	global name_input
 	username = name_input.get()
 	canvas.delete("all")
 
-	tryagain = Button(window, text="Try Again", command=game)
+	global lost
+	lost = PhotoImage(file="lose.png")
+	canvas.create_image(0, 0, image=lost, anchor="nw")
+
+	tryagain = Button(window, text="Try Again", font="OCRB 10 bold", command=game, background="grey", highlightthickness=0)
 	canvas.create_window(width/2, height/2, window=tryagain)
 
 
@@ -156,8 +184,9 @@ def running():
 	canvas.pack()
 	canvas.config(bg="black")
 	global GameOver
+	global pause
 
-	if (not p):
+	if (not p and not b):
 		GameOver = spawning_obstacles()
 
 	else:
@@ -165,13 +194,21 @@ def running():
 
 	if (GameOver):
 		window.after(100, running)
+
 	else:
 		canvas.delete("all")
 		global name_input
-		name_input = Entry(window)
+		global lost
+		lost = PhotoImage(file="lose.png")
+		canvas.create_image(0, 0, image=lost, anchor="nw")
+
+		canvas.create_text(width/2, height/2 - 60, text="You crashed the Spacecraft!\nPlease enter your name below:", font="OCRB 20 bold", fill="white")
+
+		name_input = Entry(window, background="grey", highlightthickness = 0, font="OCRB 20 bold")
 		canvas.create_window(width/2, height/2, window=name_input)
-		button = Button(window, text="OK", command = getname)
-		canvas.create_window(width/2, height/2+30, window=button) 
+
+		button = Button(window, text="OK", command = getname, highlightthickness = 0, font="OCRB 10 bold", background="grey")
+		canvas.create_window(width/2, height/2+50, window=button) 
 
 def game():
 	canvas.delete("all")
@@ -180,7 +217,7 @@ def game():
 
 	spacecraft = PhotoImage(file="character.png")
 	background = PhotoImage(file="background1.png")
-	bg = canvas.create_image(0, 0, image=background,anchor="nw")
+	bg = canvas.create_image(0, 0, image=background, anchor="nw")
 
 	global player
 	player = canvas.create_image(width/2, height/2, image=spacecraft)
@@ -195,16 +232,19 @@ def game():
 	canvas.bind("<Left>", move_left)
 	canvas.bind("<Right>", move_right)
 	canvas.bind("<Key>", pause)
+	canvas.bind("<b>",bosskey)
 	canvas.focus_set()
 
 	place_first_obstacle()
 	global ok
 	global start
 	global p
+	global b
 
 	ok = True
 	start = 1
 	p = False
+	b = False
 
 	running()
 
@@ -255,7 +295,7 @@ def start_menu():
 	#Quit Button
 	quit = Button(window, text="Quit Game", font="OCRB 10 bold", command=quitting)
 	canvas.create_window(width/2, height/2+150, window=quit)
-	
+
 
 
 
@@ -263,7 +303,7 @@ def start_menu():
 width =1280
 height =720
 window = setWindowsDimensions(width, height)
-canvas = Canvas(window, width=width, height=height)
+canvas = Canvas(window, width=width, height=height, highlightthickness=0)
 
 start_menu()
 
